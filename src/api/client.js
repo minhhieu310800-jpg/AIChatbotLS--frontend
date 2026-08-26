8041531a1ed5a19e7d472ccefcd55b0d1d52e6bb
minhhieu310800-jpg <minhhieu310800@gmail.com>
2026-08-26 23:24:57 +0700

    Change API_URL to ngrok development URL
    Updated API_URL to use ngrok URL for development.

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000"
+const API_URL = "https://unsightly-lived-polymer.ngrok-free.dev"; // "http://localhost:8000"
 
 const headers = { "ngrok-skip-browser-warning": "true" }
 
